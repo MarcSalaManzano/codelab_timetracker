7c46a04d8ed615ffaf36daf188ec13db406637f3
MarcSalaManzano <marc.salam@e-campus.uab.cat>
2020-12-29 18:08:42 +0100

    codecolab

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:codelab_timetracker/page_activities.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TimeTracker',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: TextTheme(
+            subhead: TextStyle(fontSize:20.0),
+            body1:TextStyle(fontSize:20.0)),
+      ),
+      home: PageActivities(0),
+    );
+  }
+}
---
+++ lib/page_activities.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:codelab_timetracker/PageIntervals.dart';
+import 'package:codelab_timetracker/tree.dart' hide getTree;
+// the old getTree()
+import 'package:codelab_timetracker/requests.dart';
+import 'dart:async';
+// has the new getTree() that sends an http request to the server
+class PageActivities extends StatefulWidget {
+  @override
+  _PageActivitiesState createState() => _PageActivitiesState();
+
+  int id;
+
+  PageActivities(this.id);
+}
+
+class _PageActivitiesState extends State<PageActivities> {
+  int id;
+  Future<Tree> futureTree;
+  Timer _timer;
+  static const int periodeRefresh = 6;
+  @override
+  void initState() {
+    super.initState();
+    id = widget.id; // of PageActivities
+    futureTree = getTree(id);
+    _activateTimer();
+  }
+
+  void _activateTimer() {
+    _timer = Timer.periodic(Duration(seconds: periodeRefresh), (Timer t) {
+      futureTree = getTree(id);
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Tree>(
+      future: futureTree,
+      // this makes the tree of children, when available, go into snapshot.data
+      builder: (context, snapshot) {
+        // anonymous function
+        if (snapshot.hasData) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(snapshot.data.root.name),
+              actions: <Widget>[
+                IconButton(icon: Icon(Icons.home),
+                    onPressed: () {
+                      while(Navigator.of(context).canPop()) {
+                        print("pop");
+                        Navigator.of(context).pop();
+                      }
+                      PageActivities(0);}),
+                //TODO other actions
+              ],
+            ),
+            body: ListView.separated(
+              // it's like ListView.builder() but better because it includes a separator between items
+              padding: const EdgeInsets.all(16.0),
+              itemCount: snapshot.data.root.children.length,
+              itemBuilder: (BuildContext context, int index) =>
+                  _buildRow(snapshot.data.root.children[index], index),
+              separatorBuilder: (BuildContext context, int index) =>
+              const Divider(),
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return Text("${snapshot.error}");
+        }
+        // By default, show a progress indicator
+        return Container(
+            height: MediaQuery.of(context).size.height,
+            color: Colors.white,
+            child: Center(
+              child: CircularProgressIndicator(),
+            ));
+      },
+    );
+  }
+
+  Widget _buildRow(Activity activity, int index) {
+    String strDuration = Duration(seconds: activity.duration).toString().split('.').first;
+    // split by '.' and taking first element of resulting list removes the microseconds part
+    if (activity is Project) {
+      return ListTile(
+        title: Text('${activity.name}'),
+        trailing: Text('$strDuration'),
+        onTap: () => _navigateDownActivities(activity.id),
+      );
+    } else if (activity is Task) {
+      Task task = activity as Task;
+      // at the moment is the same, maybe changes in the future
+      Widget trailing;
+      trailing = Text('$strDuration');
+
+      return ListTile(
+        title: Text('${activity.name}'),
+        trailing: trailing,
+        onTap: () => _navigateDownIntervals(activity.id),
+        onLongPress: () {
+          if ((activity as Task).active) {
+            stop(activity.id);
+            _refresh();
+            (activity as Task).active = false;
+          } else {
+            start(activity.id);
+            _refresh();
+            (activity as Task).active = true;
+          }
+        },
+      );
+    }
+  }
+
+  void _navigateDownActivities(int childId) {
+    _timer.cancel();
+    // we can not do just _refresh() because then the up arrow doesnt appear in the appbar
+    Navigator.of(context)
+        .push(MaterialPageRoute<void>(
+      builder: (context) => PageActivities(childId),
+    )).then( (var value) {
+      _activateTimer();
+      _refresh();
+    });
+  }
+
+  void _navigateDownIntervals(int childId) {
+    _timer.cancel();
+    Navigator.of(context)
+        .push(MaterialPageRoute<void>(
+      builder: (context) => PageIntervals(childId),
+    )).then( (var value) {
+      _activateTimer();
+      _refresh();
+    });
+    //https://stackoverflow.com/questions/49830553/how-to-go-back-and-refresh-the-previous-page-in-flutter?noredirect=1&lq=1
+  }
+
+  void _refresh() async {
+    futureTree = getTree(id); // to be used in build()
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    // "The framework calls this method when this State object will never build again"
+    // therefore when going up
+    _timer.cancel();
+    super.dispose();
+  }
+
+}
